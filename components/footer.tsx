ef251b0370513da4e17372af4c40d3cad018273d
Jerome Doran <disneyfan9184@gmail.com>
2025-03-07 16:39:41 -0500

    created sign-in and sign-up pages

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer className='bg-muted/50 py-12 '>
+    <footer className='bg-muted/50 py-6'>
       <div className='container mx-auto'>
-        <p className='text-center'>
+        <p className='text-center text-sm text-muted-foreground'>
           &copy; {new Date().getFullYear()} The Web Architech
         </p>
       </div>
